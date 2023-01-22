0ba1b4fac3a2824cda7c69b9656e11b4eeef3863
Alexey B.V <ablogmount@gmail.com>
2023-01-22 13:18:56 +0500

    Add line

--- index.js
+++ index.js
@@ -28,3 +28,47 @@ function draw() {
 
   // ctx.clearRect(0, 0, width, heigth);
 }
+function drawLine() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  if (canvas.getContext) {
+    ctx.beginPath(); // начать новую линию
+    ctx.moveTo(100, 50); // начать с
+    ctx.strokeStyle = 'rgb(200, 0, 0)'; // цвет линии
+    ctx.lineWidth = '5'; // ширина линии
+    ctx.lineTo(150, 150); // линия до
+    ctx.lineTo(300, 50);
+    ctx.lineTo(400, 200);
+    ctx.stroke(); // нарисовать
+
+    ctx.beginPath(); // начать новую линию
+    ctx.strokeStyle = 'rgb(0, 200, 0)'; // цвет линии
+    ctx.lineWidth = '20'; // ширина линии
+    ctx.moveTo(110, 100);
+    ctx.lineTo(390, 100);
+    ctx.lineCap = 'round'; // round, butt, square скругление линии в начале/конце
+    ctx.stroke(); // нарисовать
+  }
+}
+
+function drawTriangle() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  if (canvas.getContext) {
+    ctx.strokeStyle = 'rgb(150, 0, 0)';
+    ctx.lineWidth = '20';
+    ctx.lineCap = 'butt';
+    ctx.beginPath();
+    ctx.moveTo(50, 150);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(200, 150);
+    ctx.lineTo(50, 150);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fillStyle = 'rgb(0,0,100)';
+    ctx.fill();
+    
+  }
+}
