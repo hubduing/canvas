3be0bf6ab6cc357f549954c753867242de5cc1fc
Борисов Алексей Владимирович <bav@grdn.ru>
2023-01-24 19:02:44 +0500

    add curved lines

--- curvedLines/index.js
+++ curvedLines/index.js
@@ -1,4 +1,13 @@
-const canvas = document.getElementById('canvas');
+// const canvas = document.getElementById('canvas');
+var canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvas'))
 const ctx = canvas.getContext('2d');
 
 
+function drawCurved() {
+    ctx.moveTo(200, 50);
+    ctx.quadraticCurveTo(150, 0, 100, 50);
+    ctx.quadraticCurveTo(50, 100, 200, 280);
+    ctx.quadraticCurveTo(350, 150, 300, 50);
+    ctx.quadraticCurveTo(250, 0, 200, 50);
+    ctx.stroke();
+}
